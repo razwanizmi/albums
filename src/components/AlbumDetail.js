a2c57af225971aa476e72eefcc3e310f79abca89
Razwan Rashidi <razwan.izmi@gmail.com>
2017-06-10 16:11:04 +0800

    Add button

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { StyleSheet, View, Text, Image } from 'react-native';
+import { StyleSheet, View, Text, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image, image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
   const {
     headerContentStyle,
     headerTextStyle,
@@ -32,6 +33,11 @@ const AlbumDetail = ({ album }) => {
           source={{uri: image}}
           style={imageStyle}
         />
+      </CardSection>
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)}>
+          Buy now
+        </Button>
       </CardSection>
     </Card>
   );
---
+++ src/components/Button.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+const Button = ({ onPress, children }) => {
+  const { buttonStyle, textStyle } = styles;
+
+  return (
+    <TouchableOpacity onPress={onPress} style={buttonStyle}>
+      <Text style={textStyle}>
+        {children}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    alignSelf: 'center',
+    color: '#007AFF',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007AFF',
+    marginLeft: 5,
+    marginRight: 5
+  }
+});
+
+export default Button;
